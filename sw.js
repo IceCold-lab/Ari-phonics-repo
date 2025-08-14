49d95b32093bbc6fbf0d61067009666809aa8de7
IceCold-lab <reecerussell3@gmail.com>
2025-08-14 11:29:41 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "ari-phonics-cache-v1";
+const FILES_TO_CACHE = [
+  "/ari-phonics/",
+  "/ari-phonics/index.html",
+  "/ari-phonics/manifest.json",
+  "/ari-phonics/sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
